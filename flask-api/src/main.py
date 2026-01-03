df5de424d9953f8887f5c9a021a4cdab1ad4588f
Discover Programming <poznajprogramowanie1@gmail.com>
2026-01-03 10:47:46 +0100

    feat/2: Added basicConfig for logging.

--- flask-api/src/main.py
+++ flask-api/src/main.py
@@ -4,6 +4,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(message)s"
+)
 logging.info(f"Starting application with ENV app_version: {os.getenv('app_version')}")
 
 @app.route("/")
