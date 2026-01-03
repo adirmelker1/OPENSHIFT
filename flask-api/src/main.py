06a8afa7cad1fe7bfe42fa58b597410d45ea71b4
Discover Programming <poznajprogramowanie1@gmail.com>
2026-01-03 09:09:46 +0100

    feat/2: Changed development server to WSGI.

--- flask-api/src/main.py
+++ flask-api/src/main.py
@@ -10,5 +10,5 @@ def hello():
     return "Hello, World!"
 
 if __name__ == "__main__":
-    
-    app.run(host="0.0.0.0", port=5000)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
