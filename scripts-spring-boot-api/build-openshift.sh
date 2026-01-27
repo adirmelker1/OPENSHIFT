f9f2e084bd194a968420c6c5ea2dd4ad4de349d3
Discover Programming <poznajprogramowanie1@gmail.com>
2026-01-27 09:43:35 +0100

    feat/13: Deleted .jar file and added instruction to how obtain it.

--- scripts-spring-boot-api/build-openshift.sh
+++ scripts-spring-boot-api/build-openshift.sh
@@ -7,4 +7,8 @@ oc start-build $app_name --from-dir=../spring-boot-api/
 
 
 # Option 2 - build from ready tar
+# Before using this option - get jar from feat/12 branch and copy it
+# to spring-boot-api/build-api folder. 
+# File api-0.0.1-SNAPSHOT.jar can be downloaded from branch feat/12
+# https://gitlab.com/udemyprojects2561552/openshift-from-zero/-/tree/feat/12?ref_type=heads
 # oc start-build $app_name --from-dir=../spring-boot-api/build-api/
